5ac02e72e01da6802d4c2918abd5e7711a23e82f
mkadr5 <muhammetkadir52@gmail.com>
2023-03-25 01:04:11 +0300

    Initial Commit

---
+++ CircularSwiftUI/CircularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSwiftUIApp.swift
+//  CircularSwiftUI
+//
+//  Created by Muhammet Kadir on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
